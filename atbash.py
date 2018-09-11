37cdbfb733fa17a3a86203d01dc0b44e58170d4a
Sena Sari <sena@Senas-MBP.domain>
2018-09-10 23:25:16 -0700

    commit with pep 8 comment rules

--- atbash.py
+++ atbash.py
@@ -4,10 +4,13 @@ from ciphers import Cipher
 
 
 class Atbash(Cipher):
-    """atbash cipher encrypts the data by replacing the letter with the corresponding letter in the reversed alphabet
-    -for decryption vice versa"""
+    """atbash cipher encrypts the data by replacing the letter
+    with the corresponding letter in the reversed alphabet
+    -for decryption vice versa
+    """
 
-    atbash_dict = {forward_letter: backward_letter for forward_letter, backward_letter
+    atbash_dict = {forward_letter: backward_letter
+                   for forward_letter, backward_letter
                    in zip(string.ascii_uppercase, string.ascii_uppercase[::-1])}
 
     def encrypt(self, text):
@@ -20,7 +23,7 @@ class Atbash(Cipher):
         return ''.join(output)
 
     def decrypt(self, text):
-        """decrypts the formally encrypted text based on caesar encryption method
+        """decrypts the formally encrypted text based on caesar method
          -which is the same process with encrypting the text
          """
         return self.encrypt(text)
--- secret_messages.py
+++ secret_messages.py
@@ -3,36 +3,44 @@ from atbash import Atbash
 from caesar import Caesar
 from keyword_cipher import Keyword
 
-import os
+import sys
 
 
 def create_menu():
-    """prompt input for the user (for enc/dec & cipher choice) and store the answers to variables within the method."""
+    """prompt input for the user (for enc/dec & cipher choice)
+    and store the answers to variables within the method.
+    """
     option = input('Would you like to encrypt or decrypt your code?(E/D) ')
     option = option.upper()
     if option != 'E' and option != 'D':
         print('Invalid input! Please type E for encrypt, and D for decrypt. ')
-        os._exit(0)
+        sys.exit(0)
     print('Here is the list of ciphers: ')
     print('1. Affine Cipher')
     print('2. Atbash Cipher')
     print('3. Caesar Cipher')
     print('4. Keyword Cipher')
     print()
-    cipher_choice = input('Which cipher would you like to choose? (Please enter the number) ')
+    cipher_choice = input('Which cipher would you like to choose?'
+                          '(Enter the number) ')
     if cipher_choice not in list('1234'):
-          os._exit(0)
+        sys.exit(0)
     return option, cipher_choice
 
 
 def encode(cipher_instance, opt, message):
-    """based on the answer encrypt or decrypt the message and print the encoded message to the user."""
-    encoded = cipher_instance.encrypt(message) if opt == 'E' else cipher_instance.decrypt(message)
+    """based on the answer encrypt or decrypt the message
+     and print the encoded message to the user.
+     """
+    encoded = cipher_instance.encrypt(message) if opt == 'E' \
+        else cipher_instance.decrypt(message)
     print('Your message is: '+encoded)
 
 
 def choose(cipher, opt, messg):
-    """based on the cipher choice call the necessary class & call the encode method"""
+    """based on the cipher choice call the necessary class
+    & call the encode method
+    """
     if cipher == '1':
         af = Affine()
         encode(af, opt, messg)
@@ -53,9 +61,10 @@ secret_message = input('What message would you like to encrypt/decrypt? ')
 option1, cipher_cho = create_menu()
 choose(cipher_cho, option1, secret_message)
 
-# continuous loop of communicating to the user until they say 'nope'
+# continuous loop of communicating to the user until they type 'nope'
 while True:
-    inp = input('Anything else you want to decrypt or encrypt? (Nope/type something) ')
+    inp = input('Anything else you want to decrypt or encrypt?'
+                '(Nope/type something) ')
     if inp.lower() == 'nope':
         break
     else:
